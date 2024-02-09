7124639f9c4b54dfc6d8e6de83a529477484792a
Aman Chulawala <aman.chulawala@gmail.com>
2024-02-09 17:16:40 -0500

    Data Analytics Notebook

--- quality_utils.py
+++ quality_utils.py
@@ -1,7 +1,6 @@
 import open3d as o3d
 import numpy as np
 import copy
-import cv2
 import matplotlib.pyplot as plt
 from math import *
 
@@ -12,15 +11,15 @@ def loadPCD(filename):
 def draw_registration_result(source, target, transformation):
     source_temp = copy.deepcopy(source)
     target_temp = copy.deepcopy(target)
-    source_temp.paint_uniform_color([1, 0.706, 0])
-    target_temp.paint_uniform_color([0, 0.651, 0.929])
+    # source_temp.paint_uniform_color([1, 0.706, 0])
+    # target_temp.paint_uniform_color([0, 0.651, 0.929])
     source_temp.transform(transformation)
 
-    o3d.visualization.draw_geometries([source_temp, target_temp],
-                                      zoom=0.4459,
-                                      front=[0.9288, -0.2951, -0.2242],
-                                      lookat=[1.6784, 2.0612, 1.4451],
-                                      up=[-0.3402, -0.9189, -0.1996])
+    o3d.visualization.draw_geometries([source_temp, target_temp]) #,
+                                    #   zoom=0.4459,
+                                    #   front=[0.9288, -0.2951, -0.2242],
+                                    #   lookat=[1.6784, 2.0612, 1.4451],
+                                    #   up=[-0.3402, -0.9189, -0.1996])
     
 def preprocess_point_cloud(pcd, voxel_size):
     print(":: Downsample with a voxel size %.3f." % voxel_size)
